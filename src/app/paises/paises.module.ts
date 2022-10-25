c7104e0640ead86c103d0a44c30436c47f115661
Leyder Quintero <lele.quinter@gmail.com>
2022-10-25 17:59:38 -0500

    Formulario reactivo - primer selector

--- src/app/paises/paises.module.ts
+++ src/app/paises/paises.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { PaisesRoutingModule } from './paises-routing.module';
 import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
@@ -7,11 +8,12 @@ import { SelectorPageComponent } from './pages/selector-page/selector-page.compo
 
 @NgModule({
   declarations: [
-    SelectorPageComponent
+    SelectorPageComponent,
   ],
   imports: [
     CommonModule,
-    PaisesRoutingModule
+    PaisesRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class PaisesModule { }
